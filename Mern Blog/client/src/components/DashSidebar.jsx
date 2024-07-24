9942e068ccc8eef13f905c4e44f10a734a37e917
Divya <divyabadisa25@gmail.com>
2024-07-24 23:32:40 +0530

    Complete user image upload functionality

--- Mern Blog/client/src/components/DashSidebar.jsx
+++ Mern Blog/client/src/components/DashSidebar.jsx
@@ -15,20 +15,22 @@ export default function DashSidebar() {
     },[location.search]);
   return (
     <Sidebar className='w-full md:w-56'>
-        <Sidebar.Items>
-           <Sidebar.ItemGroup>
-                 <Link to='/dashboard?tab=profile'>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+            <Link to='/dashboard?tab=profile'>
               <Sidebar.Item 
-              active={tab === 'profile'}
-               icon={HiUser} 
-               label={'User'} 
-               labelColor='dark'>
+                active={tab === 'profile'}
+                icon={HiUser} 
+                label={'User'} 
+                labelColor='dark'
+                as='div'
+                >
                 Profile
               </Sidebar.Item>
-              </Link>
-              <Sidebar.Item  icon={HiArrowSmRight} className='cursor-pointer'>
+            </Link>
+            <Sidebar.Item  icon={HiArrowSmRight} className='cursor-pointer'>
                 Sign Out
-              </Sidebar.Item>
+            </Sidebar.Item>
            </Sidebar.ItemGroup>
         </Sidebar.Items>
 
