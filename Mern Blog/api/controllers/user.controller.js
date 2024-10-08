bd3536b151fa4e297ae0bf6b6574658db20ecf37
Divya <divyabadisa25@gmail.com>
2024-10-08 19:06:34 +0530

    add delete user api route

--- Mern Blog/api/controllers/user.controller.js
+++ Mern Blog/api/controllers/user.controller.js
@@ -8,10 +8,9 @@ export const test = ( req, res) => {
 export const updateUser = async (req, res, next) => {
   if(req.user.id !== req.params.userId){
     return next(errorHandler(403, 'you are not allowed to update this user'));
-  }
+  }  
   if (req.body.password){
     if(req.body.password.length < 6){ 
-        
       return next(errorHandler(400, 'password must be atleast 6 characters'));
     }
     req.body.password = bcryptjs.hashSync(req.body.password, 10);
@@ -45,3 +44,15 @@ export const updateUser = async (req, res, next) => {
     }
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to delete this user'));
+  }
+  try{
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json('User has been deleted');
+  } catch (error){
+    next(error);
+  }
+}
