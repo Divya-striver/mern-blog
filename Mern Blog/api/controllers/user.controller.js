77cb88e3f947dc6a0aff9b8bb191150c4cd48187
Divya <divyabadisa25@gmail.com>
2024-10-18 08:14:14 +0530

    create get users API route

--- Mern Blog/api/controllers/user.controller.js
+++ Mern Blog/api/controllers/user.controller.js
@@ -3,12 +3,12 @@ import { errorHandler } from '../utils/error.js';
 import User from '../models/user.model.js';
 
 export const test = ( req, res) => {
-    res.json({ message: 'API is working' });
+    res.json({ message: 'API is working' });    
 };
-export const updateUser = async (req, res, next) => {    
+export const updateUser = async (req, res, next) => {         
   if(req.user.id !== req.params.userId){
     return next(errorHandler(403, 'you are not allowed to update this user'));
-  }  
+  }             
   if (req.body.password){
     if(req.body.password.length < 6){ 
       return next(errorHandler(400, 'password must be atleast 6 characters'));
@@ -19,15 +19,16 @@ export const updateUser = async (req, res, next) => {
     if(req.body.username.length < 7 || req.body.username.length > 20){
        return next(errorHandler(400, 'username must be between7 and 20 characters'));
     }
-    if(req.body.username.includes(' ')){
+    if(req.body.username.includes(' ')){  
       return next(errorHandler(400, 'Username cannot contain spaces'));
     }
-    if(req.body.username !== req.body.username.toLowercase()){
+    if(req.body.username !== req.body.username.toLowerCase()){
        return next(errorHandler(400, 'username must be lowercase'));
     }
     if (!req.body.username.match(/^[a-zA-Z0-9]+$/)){
        return next(errorHandler(400, 'username can only contain letters and numbers'));
     }
+  }
     try{
       const updatedUser = await User.findByIdAndUpdate(req.params.userId, {
         $set: {
@@ -42,7 +43,6 @@ export const updateUser = async (req, res, next) => {
     }catch(error){
      next(error);
     }
-  }
 };
 
 export const deleteUser = async (req, res, next) => {
@@ -64,3 +64,44 @@ export const signout = (req, res, next) => {
     next(error);
   }
 };
+
+export const getUsers = async (req,res,next) => {
+  if(!req.user.isAdmin) {
+    return next(errorHandler(403, 'you are not allowed to see all users'));
+  }
+  try{
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === 'asc' ? 1 : -1;
+
+    const users = await User.find()
+     .sort({ createdAt: sortDirection })
+     .skip(startIndex)
+     .limit(limit);
+
+
+     const usersWithoutPassword = users.map((user) => {
+      const  { password, ...rest } = user._doc;
+      return rest;
+     });
+
+     const totalUsers = await User.countDocuments();
+     const now = new Date();
+     const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth()-1,
+      now.getDate()
+     );
+     const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+     });
+     res.status(200).json({
+      users: usersWithoutPassword,
+      totalUsers,
+      lastMonthUsers,
+     });
+
+  }catch(error){
+    next(error);
+  }
+}
